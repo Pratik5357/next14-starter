a53d78513fd4c000b7e586388a9e14fd171c5d8a
Pratik <pkkumbhar1057@gmail.com>
2024-09-22 13:01:01 +0530

    home page frontend done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,27 @@
+import Image from 'next/image';
+import styles from './home.module.css';
+
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+          <p className={styles.des}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium praesentium neque eius amet sed facilis illum. Veniam dolorum nostrum tenetur.
+          </p>
+          <div className={styles.buttons}>
+            <button className={styles.button}>Learn More</button>
+            <button className={styles.button}>Contact</button>
+          </div>
+          <div className={styles.brands}>
+            <Image src="/brands.png" alt='' fill className={styles.brandImg} />
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+            <Image src="/hero.gif" alt='' fill className={styles.heroImg} />
+          </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from './footer.module.css';
+
 const Footer = ()=> {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>Dexter</div>
+            <div className={styles.text}>
+                Dexter creative thoughts agency &copy; All rights reserved.
+            </div>
+        </div>
     )
 }
 
